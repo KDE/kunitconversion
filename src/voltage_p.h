9b71516fa7ac7024faab5e22858b3b0c1c1b22d8
Andreas Cord-Landwehr <cordlandwehr@kde.org>
2020-05-24 19:23:27 +0000

    Convert license headers to SPDX markers

--- src/voltage_p.h
+++ src/voltage_p.h
@@ -1,22 +1,9 @@
 /*
- *   Copyright (C) 2010 Petri Damstén <damu@iki.fi>
- *   Copyright (C) 2014 John Layt <jlayt@kde.org>
- *   Copyright (C) 2014 Garret Wassermann <gwasser@gmail.com>
+ *   SPDX-FileCopyrightText: 2010 Petri Damstén <damu@iki.fi>
+ *   SPDX-FileCopyrightText: 2014 John Layt <jlayt@kde.org>
+ *   SPDX-FileCopyrightText: 2014 Garret Wassermann <gwasser@gmail.com>
  *
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU Library General Public License as
- *   published by the Free Software Foundation; either version 2, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details
- *
- *   You should have received a copy of the GNU Library General Public
- *   License along with this program; if not, write to the
- *   Free Software Foundation, Inc.,
- *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *   SPDX-License-Identifier: LGPL-2.0-or-later
  */
 
 #ifndef VOLTAGE_P_H
