e1714f950c24e6e61e69630fe6ba6af301f56de8
Laurent Montel <montel@kde.org>
2021-10-29 08:26:23 +0200

    GIT_SILENT: add missing override

--- src/unit_p.h
+++ src/unit_p.h
@@ -75,7 +75,7 @@ public:
     {
     }
 
-    virtual ~CustomUnit()
+    ~CustomUnit() override
     {
     }
 };
--- src/unitcategory_p.h
+++ src/unitcategory_p.h
@@ -64,7 +64,7 @@ public:
     {
     }
 
-    virtual ~CustomCategory()
+    ~CustomCategory() override
     {
     }
 };
