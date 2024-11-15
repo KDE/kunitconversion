577c46ca9beeb7ad03a5abd3d5743fd905744134
Manuel Alcaraz Zambrano <manuelalcarazzam@gmail.com>
2024-11-15 13:14:22 +0000

    Add Python bindings
    Add Python bindings to KWidgetsAddons.
    
    Requires frameworks/extra-cmake-modules!450
    
    This is part of my GSoC project. CC @carlschwan

---
+++ python/bindings.h
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: 2024 Manuel Alcaraz Zambrano <manuelalcarazzam@gmail.com>
+// SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+
+#pragma once
+
+#include <KUnitConversion/Converter>
+#include <KUnitConversion/Unit>
+#include <KUnitConversion/UnitCategory>
+#include <KUnitConversion/Value>
